cc8c7faa7beed54e03281323c383eb0f7a472712
Rehan <rehan@rehanrana.com>
2026-05-13 19:24:35 -0400

    set version to 1.0.0rc1
    as prerelease

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "kagi-client"
-VERSION = "1.0.0"
+VERSION = "1.0.0rc1"
 PYTHON_REQUIRES = ">= 3.10"
 REQUIRES = [
     "urllib3 >= 2.1.0, < 3.0.0",
